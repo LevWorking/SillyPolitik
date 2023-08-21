689962623091088d36ae3ceeb7f30d9dae500f91
LevWorking <63475577+LevWorking@users.noreply.github.com>
2023-08-21 09:57:16 -0400

    THE BUTTON WORKS WOOO YEAH BABY

--- Assets/Scripts/Complex_Popup.cs
+++ Assets/Scripts/Complex_Popup.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using static Institution;
 
 
 public class Complex_Popup : MonoBehaviour
 {
     public GameObject buttonPrefab;
     public GameManager gameManager;
+    
+
+    public List<Institution> orgInstances = new List<Institution>();
 
     private void Start()
     {
-        
+        Institution[] scripts = FindObjectsOfType<Institution>();
+        orgInstances.AddRange(scripts);
         GenerateButtons();
     }
 
     private void GenerateButtons()
     {
-        
-        int numberOfButtons = gameManager.orgInstances.Count;
+
+        int numberOfButtons = orgInstances.Count;
         Debug.Log("Number of buttons: " + numberOfButtons);
 
         for (int i = 0; i < numberOfButtons; i++)
@@ -29,7 +34,7 @@ public class Complex_Popup : MonoBehaviour
 
             // Customize the button's properties here.
             // For example, set the button's text to the index or some meaningful value.
-            button.GetComponentInChildren<TextMeshProUGUI>().text = "Button " + (i + 1);
+            button.GetComponentInChildren<TextMeshProUGUI>().text = orgInstances[i].name.ToString();
 
             // Add a click listener to the button
             int buttonIndex = i; // Capture the value of i for the listener
